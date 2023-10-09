875dabad4e83fc9820135a89f2316d859391b05c
Matias Chomicki <matyax@gmail.com>
2023-10-09 14:35:31 +0200

    LogsContainer: add loadMore method

--- public/app/features/explore/Logs/LogsContainer.tsx
+++ public/app/features/explore/Logs/LogsContainer.tsx
@@ -33,7 +33,7 @@ import {
   selectIsWaitingForData,
   setSupplementaryQueryEnabled,
 } from '../state/query';
-import { updateTimeRange } from '../state/time';
+import { updateTimeRange, loadMore } from '../state/time';
 import { LiveTailControls } from '../useLiveTailControls';
 import { getFieldLinksForExplore } from '../utils/links';
 
@@ -104,6 +104,11 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
   onChangeTime = (absoluteRange: AbsoluteTimeRange) => {
     const { exploreId, updateTimeRange } = this.props;
     updateTimeRange({ exploreId, absoluteRange });
+  };
+
+  loadMore = (absoluteRange: AbsoluteTimeRange, refIds: string[]) => {
+    const { exploreId, loadMore } = this.props;
+    loadMore({ exploreId, absoluteRange, refIds });
   };
 
   private getQuery(
@@ -243,6 +248,7 @@ class LogsContainer extends PureComponent<LogsContainerProps, LogsContainerState
             loadingState={loadingState}
             loadLogsVolumeData={() => loadSupplementaryQueryData(exploreId, SupplementaryQueryType.LogsVolume)}
             onChangeTime={this.onChangeTime}
+            loadMore={this.loadMore}
             onClickFilterLabel={logDetailsFilterAvailable ? onClickFilterLabel : undefined}
             onClickFilterOutLabel={logDetailsFilterAvailable ? onClickFilterOutLabel : undefined}
             onStartScanning={onStartScanning}
@@ -314,6 +320,7 @@ function mapStateToProps(state: StoreState, { exploreId }: { exploreId: string }
 
 const mapDispatchToProps = {
   updateTimeRange,
+  loadMore,
   addResultsToCache,
   clearCache,
   loadSupplementaryQueryData,
