ceae0a926ae87c68f3d763868eb330c4da713c47
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-10-23 17:05:19 -0400

    update usages

--- pkg/services/ngalert/provisioning/accesscontrol.go
+++ pkg/services/ngalert/provisioning/accesscontrol.go
@@ -10,31 +10,37 @@ import (
 	"github.com/grafana/grafana/pkg/services/ngalert/store"
 )
 
-type provisioningAccessControl struct {
-	accesscontrol.RuleService
+func NewRuleAccessControlService(ac *accesscontrol.RuleService) RuleAccessControlService {
+	return &provisioningAccessControl{
+		RuleService: ac,
+	}
 }
 
-func (p provisioningAccessControl) AuthorizeAccessToRuleGroup(ctx context.Context, user identity.Requester, rules models.RulesGroup) bool {
+type provisioningAccessControl struct {
+	*accesscontrol.RuleService
+}
+
+func (p *provisioningAccessControl) AuthorizeAccessToRuleGroup(ctx context.Context, user identity.Requester, rules models.RulesGroup) bool {
 	if p.CanReadAllRules(ctx, user) {
 		return true
 	}
 	return p.RuleService.AuthorizeAccessToRuleGroup(ctx, user, rules)
 }
 
-func (p provisioningAccessControl) AuthorizeRuleChanges(ctx context.Context, user identity.Requester, change *store.GroupDelta) error {
+func (p *provisioningAccessControl) AuthorizeRuleChanges(ctx context.Context, user identity.Requester, change *store.GroupDelta) error {
 	if p.CanWriteAllRules(ctx, user) {
 		return nil
 	}
 	return p.RuleService.AuthorizeRuleChanges(ctx, user, change)
 }
 
-func (p provisioningAccessControl) CanReadAllRules(ctx context.Context, user identity.Requester) bool {
+func (p *provisioningAccessControl) CanReadAllRules(ctx context.Context, user identity.Requester) bool {
 	return p.HasAccess(ctx, user, ac.EvalAny(
 		ac.EvalPermission(ac.ActionAlertingProvisioningRead),
 		ac.EvalPermission(ac.ActionAlertingProvisioningReadSecrets),
 	))
 }
 
-func (p provisioningAccessControl) CanWriteAllRules(ctx context.Context, user identity.Requester) bool {
+func (p *provisioningAccessControl) CanWriteAllRules(ctx context.Context, user identity.Requester) bool {
 	return p.HasAccess(ctx, user, ac.EvalPermission(ac.ActionAlertingProvisioningWrite))
 }
